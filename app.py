144990fffbf8d90c44d956ba87149bdc2a1900b5
sam505 <mainasamuel505@gmail.com>
2021-12-13 08:15:02 +0300

    Update files

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from dash import dcc
 from dash import html
 import pandas as pd
 import plotly.express as px
+import plotly.figure_factory as ff
 import json
 from dash.dependencies import Input, Output
 
 data_jurisdiction = pd.read_csv(r"COVID-19_Vaccinations_in_the_United_States_Jurisdiction.csv")
 data_county = pd.read_csv(r"COVID-19_Vaccinations_in_the_United_States_County.csv")
+data_county = data_county[data_county.Recip_State != "UNK"]
 data_original = pd.read_csv(r"United_States_COVID-19_County_Level_of_Community_Transmission_as_Originally_Posted.csv")
-dates = list(data_county.Date.unique())
+dates = data_county.Date.unique().tolist()
+orig_dates = list(data_original.report_date.unique())
 indicators_state = list(data_county.Recip_State.unique())
 indicators_county = list(data_county.Recip_County.unique())
 dates.reverse()
@@ -20,8 +23,7 @@ points = 10
 for m in range(10):
     markers[int(length / points * m)] = dates[int(length / points * m)]
 
-with open('geojson-counties-fips.json') as json_data:
-    counties = json.load(json_data)
+counties = json.load(open('geojson-counties-fips.json'))
 
 app = Dash(__name__)
 app.layout = html.Div(
@@ -38,7 +40,7 @@ app.layout = html.Div(
         dcc.Slider(id='input-2',
                    min=0,
                    max=len(dates),
-                   value=1,
+                   value=30,
                    marks=markers),
         html.Hr(),
         html.Br(),
@@ -54,7 +56,9 @@ app.layout = html.Div(
                                         {'label': 'Single Dose', 'value': 'Administered_Dose1_Pop_Pct'}, ],
                                value='Series_Complete_Pop_Pct',
                                labelStyle={'display': 'inline-block'}),
-                dcc.Graph(id='graph-2'),
+                dcc.Graph(
+                    id='graph-2'
+                ),
                 dcc.Slider(id='input-6',
                            min=0,
                            max=len(dates),
@@ -65,18 +69,20 @@ app.layout = html.Div(
             html.Div([
                 dcc.Dropdown(
                     id='input-4',
-                    options=[{'label': i, 'value': i} for i in indicators_county],
-                    value=indicators_county[0],
-                    style={"width": "50%"}
+                    options=[],
+                    style={"width": "50%"},
+                    value=""
                 ),
                 html.Br(),
                 dcc.RangeSlider(
-                        id='input-7',
-                        min=0,
-                        max=20,
-                        step=0.5,
-                        value=[5, 15]
-                    ),
+                    id='input-7',
+                    min=0,
+                    max=100,
+                    step=0.5,
+                    value=[0, 5],
+                    marks={},
+                    allowCross=False
+                ),
                 dcc.Graph(id='graph-3'),
                 dcc.Graph(id='graph-4')
             ], className="six columns")],
@@ -87,18 +93,32 @@ app.layout = html.Div(
 
 
 @app.callback(
-    Output(component_id='graph-1', component_property='figure'),
-    Output(component_id='graph-2', component_property='figure'),
-    Input(component_id='input-1', component_property='value'),
-    Input(component_id='input-2', component_property='value'),
+    Output(component_id='input-4', component_property='options'),
+    Output(component_id='input-7', component_property='marks'),
     Input(component_id='input-3', component_property='value'),
-    Input(component_id='input-4', component_property='value'),
-    Input(component_id='input-5', component_property='value'),
-    Input(component_id='input-6', component_property='value'),
-    Input(component_id='input-7', component_property='value')
+    Input(component_id='input-4', component_property='value')
 )
-def function_one(input_1, input_2, input_3, input_4, input_5, input_6, input_7):
-    fig1 = px.choropleth(data_county[data_county.Date == dates[input_2]], geojson=counties,
+def get_counties(input_3, input_4):
+    county_s = data_county[data_county.Recip_State == input_3]
+    county_s = county_s.Recip_County.unique().tolist()
+    county_list = [{'label': i, 'value': i} for i in county_s]
+    data_positivity = data_original[data_original.county_name == input_4]
+    positivity_dates = data_positivity.report_date.unique().tolist()
+    markers_orig = {}
+    length_orig = len(positivity_dates)
+    for n in range(10):
+        markers_orig[int(length_orig / points * n)] = dates[int(length_orig / points * n)]
+    return county_list, markers_orig
+
+
+@app.callback(
+    Output(component_id='graph-1', component_property='figure'),
+    Input(component_id='input-1', component_property='value'),
+    Input(component_id='input-2', component_property='value')
+)
+def choropleth_1(input_1, input_2):
+    data_choropleth1 = data_county[data_county.Date == dates[input_2]]
+    fig1 = px.choropleth(data_choropleth1,
                          locations='Recip_State',
                          locationmode='USA-states',
                          color=input_1,
@@ -106,16 +126,70 @@ def function_one(input_1, input_2, input_3, input_4, input_5, input_6, input_7):
                          color_continuous_scale='Viridis',
                          scope='usa',
                          labels={input_1: "% vaccinated"})
-    fig2 = px.choropleth(data_county[data_county.Recip_State == input_3], geojson=counties,
-                         locations='Recip_County',
-                         locationmode='USA-states',
-                         color=input_5,
-                         range_color=[0, 100],
-                         color_continuous_scale='Viridis',
-                         scope='usa',
-                         labels={input_1: "% vaccinated"})
-    return fig1.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0}),\
-           fig2.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
+    return fig1.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
+
+
+@app.callback(
+    Output(component_id='graph-2', component_property='figure'),
+    Input(component_id='input-3', component_property='value'),
+    Input(component_id='input-5', component_property='value'),
+    Input(component_id='input-6', component_property='value'),
+)
+def choropleth_2(input_3, input_5, input_6):
+    data_choropleth2 = data_county[data_county.Date == dates[input_6]]
+    data_choropleth2 = data_choropleth2[data_choropleth2.Recip_State == input_3]
+    fig = px.choropleth(
+        data_choropleth2,
+        range_color=[0, 100],
+        locations="FIPS",
+        geojson=counties,
+        color=input_5,
+        scope="usa"
+                        )
+    fig.update_geos(fitbounds="locations", visible=False)
+    return fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
+
+
+@app.callback(
+    Output(component_id='graph-3', component_property='figure'),
+    Input(component_id='input-4', component_property='value'),
+    Input(component_id='input-7', component_property='value')
+)
+def positivity_1(input_4, input_7):
+    data_positivity = data_original[data_original.county_name == input_4]
+    pos_length = len(data_positivity)
+    data_positivity.reset_index(inplace=True)
+    data_positivity = data_positivity.loc[pos_length / 100 * int(input_7[0]):pos_length / 100 * int(input_7[1])]
+    data_positivity["percent_test_results_reported_positive_last_7_days"] = pd.to_numeric(
+        data_positivity["percent_test_results_reported_positive_last_7_days"], errors='coerce')
+    data_positivity = data_positivity[data_positivity.percent_test_results_reported_positive_last_7_days.notna()]
+    fig = px.line(
+        data_positivity,
+        "report_date",
+        "percent_test_results_reported_positive_last_7_days"
+    )
+    return fig
+
+
+@app.callback(
+    Output(component_id='graph-4', component_property='figure'),
+    Input(component_id='input-4', component_property='value'),
+    Input(component_id='input-7', component_property='value')
+)
+def positivity_2(input_4, input_7):
+    data_positivity = data_original[data_original.county_name == input_4]
+    pos_length = len(data_positivity)
+    data_positivity.reset_index(inplace=True)
+    data_positivity = data_positivity.loc[pos_length/100*int(input_7[0]):pos_length/100*int(input_7[1])]
+    data_positivity["cases_per_100K_7_day_count_change"] = pd.to_numeric(
+        data_positivity["cases_per_100K_7_day_count_change"], errors='coerce')
+    data_positivity = data_positivity[data_positivity.cases_per_100K_7_day_count_change.notna()]
+    fig = px.line(
+        data_positivity,
+        "report_date",
+        "cases_per_100K_7_day_count_change"
+    )
+    return fig
 
 
 if __name__ == "__main__":
